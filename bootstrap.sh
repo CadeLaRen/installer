f9a16d09d117d28c087a4ae38ab35d0944839445
Sven Dowideit <SvenDowideit@home.org.au>
2016-07-25 07:26:13 +0000

    restart image with --net host to hopefully allow it to upgrade services too
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,32 +7,44 @@ if [[ ! -d "/host" ]]; then
 	exit 255
 fi
 
-CMD="$1"
-if [[ -z $CMD ]]; then
-	CMD="install"
+# make sure we have the --network host
+CONTAINERNAME=`cat /etc/hostname`
+HOSTNAME=`cat /host/etc/hostname`
+echo "test $HOSTNAME vs $CONTAINERNAME"
+if [[ "$HOSTNAME" != "$CONTAINERNAME" ]]; then
+	echo "$HOSTNAME ne $CONTAINERNAME, restarting with --net host"
+	# have to assume the hostname == containername, so we can use `docker inspect`
+	IMAGENAME=`chroot /host docker inspect --format "{{ .Config.Image }}" $(hostname)`
+
+	chroot /host docker run --net host -v /:/host --rm -it $IMAGENAME $@
+else
+	CMD="$1"
+	if [[ -z $CMD ]]; then
+		CMD="install"
+	fi
+
+	CMDLINE="/install/$CMD"
+	if [[ ! -e "$CMDLINE" ]]; then
+		echo ""
+		echo "ERROR: No command '$CMD' found"
+		echo ""
+		/install/help $@
+		exit 254
+	fi
+
+	# remove $CMD from argc
+	shift
+
+	DIR=$(mktemp -d -p /host)
+	cp -r /install/* $DIR
+
+	# Need to know the tmpdir inside the chroot too
+	HOSTDIR=$(echo $DIR | sed "s/\/host//")
+	echo "entering host chroot running '$CMD $@'"
+
+	# can't use exec, as we'd like to remove the temp dir from the host FS
+	chroot /host "$HOSTDIR/$CMD" $@
+
+	echo "cleaning up tmpdir"
+	rm -r $DIR
 fi
-
-CMDLINE="/install/$CMD"
-if [[ ! -e "$CMDLINE" ]]; then
-	echo ""
-	echo "ERROR: No command '$CMD' found"
-	echo ""
-	/install/help $@
-	exit 254
-fi
-
-# remove $CMD from argc
-shift
-
-DIR=$(mktemp -d -p /host)
-cp -r /install/* $DIR
-
-# Need to know the tmpdir inside the chroot too
-HOSTDIR=$(echo $DIR | sed "s/\/host//")
-echo "entering host chroot running '$CMD $@'"
-
-# can't use exec, as we'd like to remove the temp dir from the host FS
-chroot /host "$HOSTDIR/$CMD" $@
-
-echo "cleaning up tmpdir"
-rm -r $DIR
